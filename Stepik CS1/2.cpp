52f6217dfb94c2c0b9296a2ff76236be2fcc0124
alevenskiy <ppcrn97@gmail.com>
2021-04-06 17:19:56 +0300

    test

--- Stepik CS1/2.cpp
+++ Stepik CS1/2.cpp
@@ -6,12 +6,12 @@ using namespace std;
 Требования к реализации: в данном задании запрещено пользоваться циклами*/
 unsigned gcd(unsigned a, unsigned b)
 {
-    if (a == 0 || b == 0)
-        return (a + b);
-    else if (a > b)
-        return gcd(a % b, b);
-    else
-        return gcd(a, b % a);
+	if (a == 0 || b == 0)
+		return (a + b);
+	else if (a > b)
+		return gcd(a % b, b);
+	else
+		return gcd(a, b % a);
 }
 
 /*2.2.10
@@ -21,11 +21,11 @@ unsigned gcd(unsigned a, unsigned b)
 Требуется вывести эту последовательность в обратном порядке.
 Требования к реализации: в данном задании запрещено использовать циклы, а также дополнительную память: массивы, строки или контейнеры (даже, если вы с ними уже знакомы)*/
 void meow() {
-    int c = 0;
-    cin >> c;
-    if (c == 0) return;
-    meow();
-    cout << c << ' ';
+	int c = 0;
+	cin >> c;
+	if (c == 0) return;
+	meow();
+	cout << c << ' ';
 }
 
 /*2.3.9
@@ -36,12 +36,12 @@ void meow() {
 Требования к реализации: вам нужно реализовать только функцию циклического сдвига.*/
 void myrotate(int a[], unsigned size, int shift)
 {   
-    int b[sizeof(a)] = {};
-    for (int i = 0; i < size; i++)
-        b[(size + i - shift) % size] = a[i];
+	int b[sizeof(a)] = {};
+	for (int i = 0; i < size; i++)
+		b[(size + i - shift) % size] = a[i];
 
-    for (int i = 0; i < size; i++)
-        a[i] = b[i];
+	for (int i = 0; i < size; i++)
+		a[i] = b[i];
 
 }
 
@@ -49,72 +49,72 @@ void myrotate(int a[], unsigned size, int shift)
 Напишите функцию, которая вычисляет длину C-style строки*/
 unsigned mystrlen(const char* str)
 {
-    const char* str_end = str;
-    while (*str_end != '\0') {
-        str_end++;
-    }
-    return str_end - str;
+	const char* str_end = str;
+	while (*str_end != '\0') {
+		str_end++;
+	}
+	return str_end - str;
 }
 
 /*2.4.7
 Вам требуется реализовать функцию конкатенации (склейки) двух C-style строк*/
 void mystrcat(char* to, const char* from)
 {
-    while (*to != '\0') to++;
+	while (*to != '\0') to++;
 
-    while (*from != '\0') {
-        *to = *from;
-        to++;
-        from++;
-    }
-    *to = '\0';
+	while (*from != '\0') {
+		*to = *from;
+		to++;
+		from++;
+	}
+	*to = '\0';
 }
 
 /*2.4.8
 Напишите функцию поиска первого вхождения шаблона в текст*/
 int mystrstr(const char* text, const char* pattern)
 {
-    if (*pattern == '\0') return 0;
-    int position = 0;
-    //for(const char* ind = pattern; *text != '\0'; text++){
-    while (*text != '\0') {
-        const char* ind = pattern;
-        const char* ind_text = text;
-        while (*ind == *ind_text) {
-            ind++;
-            ind_text++;
-            if (*ind == '\0')
-                return position;
-        }
-        text++;
-        position++;
-    }
-    return -1;
+	if (*pattern == '\0') return 0;
+	int position = 0;
+	//for(const char* ind = pattern; *text != '\0'; text++){
+	while (*text != '\0') {
+		const char* ind = pattern;
+		const char* ind_text = text;
+		while (*ind == *ind_text) {
+			ind++;
+			ind_text++;
+			if (*ind == '\0')
+				return position;
+		}
+		text++;
+		position++;
+	}
+	return -1;
 }
 
 
 char* myresize(const char* str, unsigned size, unsigned new_size) {
-    char* new_str = new char[new_size];
-    for (int i = 0; i < size && i < new_size; i++)
-        new_str[i] = str[i];
-    delete[] str;
-    return new_str;
+	char* new_str = new char[new_size];
+	for (int i = 0; i < size && i < new_size; i++)
+		new_str[i] = str[i];
+	delete[] str;
+	return new_str;
 }
 
 char* mygetline() {
-    unsigned sizestr = 8;
-    char* str = new char[sizestr];
-    unsigned size = 0;
+	unsigned sizestr = 8;
+	char* str = new char[sizestr];
+	unsigned size = 0;
 
-    char c = '\0';
-    while (cin.get(c) && c != '\n') {
-        str[size] = c;
-        size++;
-        if (size == sizestr - 1) {
-            str = myresize(str, sizestr, sizestr * 2);
-            sizestr *= 2;
-        }
-    }
-    str[size] = '\0';
-    return str;
+	char c = '\0';
+	while (cin.get(c) && c != '\n') {
+		str[size] = c;
+		size++;
+		if (size == sizestr - 1) {
+			str = myresize(str, sizestr, sizestr * 2);
+			sizestr *= 2;
+		}
+	}
+	str[size] = '\0';
+	return str;
 }
